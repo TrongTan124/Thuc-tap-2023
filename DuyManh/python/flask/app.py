2936ea51d94a8b92ab331a04fc1ecab56f514705
25FM <ddmanh17602@gmail.com>
2023-06-05 11:14:02 +0700

    create document flask, update app config
    document flask: Configure Flask-SQLAlchemy with Flask application
    
    update app config: config migrate

--- DuyManh/python/flask/app.py
+++ DuyManh/python/flask/app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate
 app = Flask(__name__)
+
+# config database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/MANH/Documents/GitHub/Thuc-tap-2023/DuyManh/python/flask/templates/test.db'
+
+# config migrate
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,9 +19,18 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Task %r>' % self.id
+
     
 with app.app_context():
     db.create_all()
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -33,7 +48,6 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
-
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
@@ -41,25 +55,24 @@ def delete(id):
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
-        return redirect('../')
+        return redirect('/')
     except:
-        return 'There was an issue delete your task'
-
+        return 'There was an issue deleting your task'
 
 @app.route('/update/<int:id>', methods= ['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
     if request.method == 'POST':
         task.content = request.form['content']
-        
+
         try:
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue update your task'
+            return 'There was an issue updating your task'
     else:
-        return render_template('update.html', task = task) 
-
+        return render_template('update.html', task=task)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
