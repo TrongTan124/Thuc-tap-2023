a0d0096f62b8b276fed6f54e7960242d84ed4fb5
HidroDatz <hidrodat2002@gmail.com>
2023-06-13 09:54:41 +0700

    turn route folder to api folder

--- Nguyen thanh dat/python/book CRUD/api/book_route.py
+++ Nguyen thanh dat/python/book CRUD/api/book_route.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, session, request
-from Model.book import Book,db
+from model.book import Book,db
 
 book_bp = Blueprint("book_route", __name__, template_folder="templates")
 
--- Nguyen thanh dat/python/book CRUD/app.py
+++ Nguyen thanh dat/python/book CRUD/app.py
@@ -3,14 +3,14 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from Model.book import db
-from route.book_route import book_bp
-from route.list_route import python_list_bp
+from model.book import db
+from api.book_route import book_bp
+from api.list_route import python_list_bp
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = "hidrodat"
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:123@127.0.0.1:4449/book_store"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL")
 app.register_blueprint(book_bp)
 app.register_blueprint(python_list_bp)
 db.init_app(app) 
